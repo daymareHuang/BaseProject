76f8d8adeddd7fc9f1d0fd1d163103779f49d0ec
daymareHuang <32323246+daymareHuang@users.noreply.github.com>
2018-01-16 13:46:09 +0800

    GoogleSearch change

--- src/Software Engineering Topic/src/GoogleScraperDemo.java
+++ src/Software Engineering Topic/src/GoogleScraperDemo.java
@@ -8,22 +8,34 @@ public class GoogleScraperDemo {
   private String answer;
   private Elements links;
   
-  public void googlesearch() throws JauntException{    
-    UserAgent userAgent = new UserAgent();      //create new userAgent (headless browser)
+  public void setKeyword() throws NotEnglishException {
     Scanner scanner = new Scanner(System.in);
+    
     System.out.println("請輸入你所想要查的資料(以英文輸入): ");
     answer = scanner.nextLine();
-    System.out.println("你所要的資料如下:\n");
-    
- 
-    
-    userAgent.settings.autoSaveAsHTML = true;
-    userAgent.visit("http://google.com?hl=en");          //visit the english version of google 
-    userAgent.doc.apply(answer);         //apply form input (starting at first editable field)
-    userAgent.doc.submit("Google Search");      //click submit button labelled "Google Search"
-    
-    links = userAgent.doc.findEvery("<h3 class=r>").findEvery("<a>");   //find search result links 
-    for(Element link : links) System.out.println(link.getAt("href"));            //print results
-    
+    if(!answer.matches("[a-zA-Z0-9|\\.]*")) {
+      throw new NotEnglishException();
+    }
   }
+  public String getKeyword() {
+    return answer;
+  }
+  
+  public void GoogleSearch() throws JauntException {    
+    UserAgent userAgent = new UserAgent();      //create new userAgent (headless browser)
+    
+      System.out.println("你所要的資料如下:\n");
+      userAgent.settings.autoSaveAsHTML = true;
+      userAgent.visit("http://google.com?hl=en");          //visit the english version of google 
+      userAgent.doc.apply(answer);                         //apply form input (starting at first editable field)
+      userAgent.doc.submit("Google Search");               //click submit button labelled "Google Search"
+      
+      links = userAgent.doc.findEvery("<h3 class=r>").findEvery("<a>");            //find search result links 
+     
+  }
+  
+  public void showGoogleSearchResult() throws JauntException {
+    for(Element link : links) System.out.println(link.getAt("href"));            //print results
+  }
+  
 }
